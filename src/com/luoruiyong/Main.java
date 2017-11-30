d796fa6279f4b20eddc399f4bac13705f4e07b00
luoruiyong <1217151349@qq.com>
2017-12-01 01:49:56 +0800

    【Modify】 测试修改

--- src/com/luoruiyong/Main.java
+++ src/com/luoruiyong/Main.java
@@ -1,5 +1,10 @@
 package com.luoruiyong;
 
+/**
+ * ³ÌĞòÈë¿Ú
+ * @author Administrator
+ *
+ */
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
